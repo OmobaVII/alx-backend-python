181726ca742b3055202b388b870a7a16ee61d6fb
OmobaVII <omoba.career@gmail.com>
2023-10-28 10:44:20 +0000

    Implememnt the basic of async

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+This module provide a function `wait_random`
+this function shows the basic implementation
+of async
+"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """an asynchronous coroutine that takes in an integer argument
+    with a default of 10"""
+    rand_float = random.uniform(0, max_delay)
+    await asyncio.sleep(rand_float)
+    return rand_float
