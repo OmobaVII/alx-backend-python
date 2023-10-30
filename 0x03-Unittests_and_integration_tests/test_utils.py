8c4ada1c3169b095a6f383dc3c3ffd25a2053ab3
OmobaVII <omoba.career@gmail.com>
2023-10-30 16:33:15 +0000

    Parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -15,7 +15,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_accessnestedmap(self, nested_map, path, expected_result):
+    def test_access_nested_map(self, nested_map, path, expected_result):
         """test for access_nested_map"""
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
 
