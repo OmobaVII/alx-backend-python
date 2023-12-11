feb01d0842819afff88a2d9827f69a57ba27b063
OmobaVII <omoba.career@gmail.com>
2023-12-11 10:45:34 +0000

    Basic annotations -add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -3,6 +3,7 @@
     This is a module that provides a function `add` used to add two numbers.
 """
 
+
 def add(a: float, b: float) -> float:
     """
         This function adds two numbers and returns the result.
